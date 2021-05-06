ab901214cb2fee02795e1561f1cec9d2f651b9f8
AYOKINYA <ayokinya@gmail.com>
2021-05-07 00:46:04 +0900

    [FIX] GET permitted to Posts and Comments

--- src/main/java/com/clone/springbootredditbackend/config/SecurityConfig.java
+++ src/main/java/com/clone/springbootredditbackend/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.clone.springbootredditbackend.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -44,6 +45,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         httpSecurity.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/api/auth/**")
+                .permitAll()
+                .antMatchers(HttpMethod.GET, "/api/subreddit")
+                .permitAll()
+                .antMatchers(HttpMethod.GET, "/api/posts/")
+                .permitAll()
+                .antMatchers(HttpMethod.GET, "/api/posts/**")
+                .permitAll()
+                .antMatchers(HttpMethod.GET, "/api/comments/**")
                 .permitAll()
                 .antMatchers("/v2/api-docs",
                         "/configuration/ui",
--- src/main/java/com/clone/springbootredditbackend/mapper/PostMapper.java
+++ src/main/java/com/clone/springbootredditbackend/mapper/PostMapper.java
@@ -5,10 +5,15 @@ import com.clone.springbootredditbackend.service.AuthService;
 import com.clone.springbootredditbackend.web.dto.PostRequest;
 import com.clone.springbootredditbackend.web.dto.PostResponse;
 import com.github.marlonlom.utilities.timeago.TimeAgo;
-import lombok.RequiredArgsConstructor;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+import static com.clone.springbootredditbackend.domain.VoteType.DOWNVOTE;
+import static com.clone.springbootredditbackend.domain.VoteType.UPVOTE;
 
 @Mapper(componentModel = "spring")
 public abstract class PostMapper {
@@ -24,6 +29,7 @@ public abstract class PostMapper {
     @Mapping(target = "subreddit", source = "subreddit")
     @Mapping(target = "description", source = "postRequest.description")
     @Mapping(target = "voteCount", constant = "0")
+    @Mapping(target = "user", source = "user")
     public abstract Post map(PostRequest postRequest, Subreddit subreddit, User user);
 
     @Mapping(target = "id", source = "postId")
@@ -35,6 +41,8 @@ public abstract class PostMapper {
     @Mapping(target = "voteCount", source = "voteCount")
     @Mapping(target = "commentCount", expression = "java(commentCount(post))")
     @Mapping(target = "duration", expression = "java(getDuration(post))")
+    @Mapping(target = "upVote", expression = "java(isPostUpVoted(post))")
+    @Mapping(target = "downVote", expression = "java(isPostDownVoted(post))")
     public abstract PostResponse mapToDto(Post post);
 
     Integer commentCount(Post post) {
@@ -42,6 +50,22 @@ public abstract class PostMapper {
     }
     String getDuration(Post post) {
         return TimeAgo.Companion.using(post.getCreatedDate().toEpochMilli());
+    }
+    boolean isPostUpVoted(Post post) {
+        return checkVoteType(post, UPVOTE);
+    }
+    boolean isPostDownVoted(Post post) {
+        return checkVoteType(post, DOWNVOTE);
+    }
+
+    private boolean checkVoteType(Post post, VoteType voteType) {
+        if (authService.isLoggedIn()) {
+            Optional<Vote> voteForPostByUser = voteRepository.findTopByPostAndUserOrderByVoteIdDesc(post,
+                    authService.getCurrentUser());
+            return voteForPostByUser.filter(vote -> vote.getVoteType().equals(voteType))
+                    .isPresent();
+        }
+        return false;
     }
 
 }
